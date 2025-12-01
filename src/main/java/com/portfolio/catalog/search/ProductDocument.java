bf6966cd0a5c3daa3c699c497e9148ee01f5bbe6
roralo <roberto.rajo.lopez@gmail.com>
2025-12-01 09:43:19 +0100

    Add Lombok constructors to ProductDocument

--- src/main/java/com/portfolio/catalog/search/ProductDocument.java
+++ src/main/java/com/portfolio/catalog/search/ProductDocument.java
@@ -1,7 +1,9 @@
 package com.portfolio.catalog.search;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -16,6 +18,8 @@ import java.util.UUID;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDocument {
 
     @Id
